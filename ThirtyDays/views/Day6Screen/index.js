3afdea985ee20eec49aceddcdbeed23acc88fddd
BengBu-YueZhang <zhangyue_coder@163.com>
2018-12-07 11:03:14 +0800

    update

--- ThirtyDays/views/Day6Screen/index.js
+++ ThirtyDays/views/Day6Screen/index.js
@@ -1,8 +1,38 @@
 import React from 'react'
-import { View, PanResponder, StyleSheet } from 'react-native'
+import { View, PanResponder, StyleSheet, Text } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
+// import RnC from './components/RnC'
+// import RnD from './components/RnD'
+// import randomcolor from 'randomcolor'
+// import uuid from 'uuid/v1'
+
+const styles = StyleSheet.create(
+  {
+    root: {
+      width: '100%',
+      height: 60,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  }
+)
 
 class Day6Screen extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  componentDidMount () {
+  }
+
+  render () {
+    return (
+      <View style={{paddingTop: 20}}>
+        <Text>123</Text>
+      </View>
+    )
+  }
 }
 
 const Day6Stack = createStackNavigator(
