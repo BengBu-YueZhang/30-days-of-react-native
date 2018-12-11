cd73bec554329f422e59d690a16fd557d3529c77
BengBu-YueZhang <zhangyue_coder@163.com>
2018-12-11 13:14:11 +0800

    update

--- ThirtyDays/views/Day8Screen/components/Menu.js
+++ ThirtyDays/views/Day8Screen/components/Menu.js
@@ -4,23 +4,55 @@ import {
   StyleSheet,
   Animated,
   Modal,
-  Text
+  Text,
+  Image
 } from 'react-native'
 import PropTypes from 'prop-types'
 
-console.log(VibrancyView)
 
 const styles = StyleSheet.create(
   {
     root: {
       width: '100%',
       height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.8)'
+      backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
+    },
+    content: {
+      width: 300,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    },
+    icon: {
+      width: 110,
+      height: 110
+    },
+    button: {
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginBottom: 10,
+      padding: 20
+    },
+    buttonText: {
+      color: '#fff',
+      lineHeight: 24,
+      fontSize: 14
     }
   }
 )
 
 class Menu extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      animation: new Animated.Value(1)
+    }
+  }
+
   render () {
     const { modalVisible } = this.props
     return (
@@ -28,7 +60,34 @@ class Menu extends React.Component {
         transparent={true}
         visible={modalVisible}
       >
-        <View style={styles.root}></View>
+        <View style={styles.root}>
+          <View style={styles.content}>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-audio.png')}/>
+              <Text style={styles.buttonText}>Audio</Text>
+            </View>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-chat.png')}/>
+              <Text style={styles.buttonText}>Chat</Text>
+            </View>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-link.png')}/>
+              <Text style={styles.buttonText}>Link</Text>
+            </View>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-photo.png')}/>
+              <Text style={styles.buttonText}>Photo</Text>
+            </View>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-quote.png')}/>
+              <Text style={styles.buttonText}>Quote</Text>
+            </View>
+            <View style={styles.button}>
+              <Image style={styles.icon} source={require('../../../assets/tumblr-text.png')}/>
+              <Text style={styles.buttonText}>Text</Text>
+            </View>
+          </View>
+        </View>
       </Modal>
     )
   }
