42c6454b60e566028358050ada4166e6b3ba1261
BengBu-YueZhang <zhangyue_coder@163.com>
2019-02-07 10:10:11 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,73 +1,18 @@
 # 30-days-of-react-native
 
-30天学习ReactNative
+30天学习ReactNative, [灵感来自](https://github.com/fangwei716/30-days-of-react-native), 目前开启v2分支, 重写该项目以支持安卓。
 
 ```shell
+
 git clone https://github.com/BengBu-YueZhang/30-days-of-react-native.git
 
 yarn install
 
-# 目前没有适配安卓
 react-native run-ios
+
+react-native run-android
 ```
 
-## Day1
+## 第零天
 
-> 使用react-navigation制作的底部导航
-
-![image](https://i.loli.net/2018/11/29/5bff421c69667.gif)
-
-## Day2
-
-> 结合redux的秒表
-
-![image](https://i.loli.net/2018/12/02/5c03b939f0798.gif)
-
-## Day3
-
-> 推特入场动画
-
-![image](https://i.loli.net/2018/12/04/5c062270036bb.png)
-
-### Day4
-
-> 使用PanResponder进行单指操作
-
-![image](https://i.loli.net/2018/12/03/5c04bfabf386d.gif)
-
-
-### Day5
-
-> 侧边菜单
-
-![image](https://raw.githubusercontent.com/fangwei716/ThirtyDaysOfReactNative/screenshots/screenshot/day8.gif)
-
-### Day6
-
-> 拖拽排序列表
-
-### Day7
-
-> 模仿豆瓣的照片选择器
-
-![image](https://i.loli.net/2018/12/10/5c0e4883d9aa5.png)
-
-![image](https://i.loli.net/2018/12/10/5c0e70e30927d.png)
-
-### Day8
-
-![image](https://raw.githubusercontent.com/fangwei716/ThirtyDaysOfReactNative/screenshots/screenshot/day10.gif)
-
-### Day9 手势解锁
-
-![image](https://raw.githubusercontent.com/fangwei716/ThirtyDaysOfReactNative/screenshots/screenshot/day16.gif)
-
-### Day10 Youtube可滚动标签
-
-### Day11 Twitter用户页面
-
-### Day12 touchID
-
-### Day13 Google Now
-
-### Day14 RN多选图片上传
+## 第一天
--- ThirtyDays/App.js
+++ ThirtyDays/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import HomeScreen from './views/HomeScreen'
-import Day1Stack from './views/Day1Screen'
-import Day2Stack from './views/Day2Screen'
-import Day3Stack from './views/Day3Screen'
-import Day4Stack from './views/Day4Screen'
-import Day5Stack from './views/Day5Screen'
-import Day6Stack from './views/Day6Screen'
-import Day7Stack from './views/Day7Screen'
-import Day8Stack from './views/Day8Screen'
-import Day9Stack from './views/Day9Screen'
+// import Day1Stack from './views/Day1Screen'
+// import Day2Stack from './views/Day2Screen'
+// import Day3Stack from './views/Day3Screen'
+// import Day4Stack from './views/Day4Screen'
+// import Day5Stack from './views/Day5Screen'
+// import Day6Stack from './views/Day6Screen'
+// import Day7Stack from './views/Day7Screen'
+// import Day8Stack from './views/Day8Screen'
+// import Day9Stack from './views/Day9Screen'
 import {
   createStackNavigator,
   createAppContainer,
@@ -34,15 +34,15 @@ const HomeStack = createStackNavigator(
 const RootStack = createSwitchNavigator(
   {
     Home: HomeStack,
-    Day1: Day1Stack,
-    Day2: Day2Stack,
-    Day3: Day3Stack,
-    Day4: Day4Stack,
-    Day5: Day5Stack,
-    Day6: Day6Stack,
-    Day7: Day7Stack,
-    Day8: Day8Stack,
-    Day9: Day9Stack
+    // Day1: Day1Stack,
+    // Day2: Day2Stack,
+    // Day3: Day3Stack,
+    // Day4: Day4Stack,
+    // Day5: Day5Stack,
+    // Day6: Day6Stack,
+    // Day7: Day7Stack,
+    // Day8: Day8Stack,
+    // Day9: Day9Stack
   },
   {
     initialRouteName: 'Home',
